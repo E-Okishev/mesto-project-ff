8752ad4f9bec11ac2218f834d3b02d0a2c2ac834
Egor Okishev <e-okishev@mail.ru>
2024-04-08 23:06:52 +0300

    add create card function

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,41 @@
 // @todo: Темплейт карточки
+const cardTemp = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const places = document.querySelector('.places')
+const placesList = places.querySelector('.places__list');
+const deleteButton = placesList.querySelector('.card__delete-button')
 
 // @todo: Функция создания карточки
+function createCard(image, title) {
+  const cardElement = cardTemp.querySelector('.card').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = image;
+  cardElement.querySelector('.card__title').textContent = title;
+  cardElement.querySelector('.card__image').alt = title;
+
+  deleteButton.addEventListener('click', delCard)
+
+  placesList.append(cardElement);
+}
 
 // @todo: Функция удаления карточки
+function delCard() {
+  const cards = document.querySelectorAll('.card');
+
+  for (let card of cards) {
+    card.addEventListener('click', function () {
+      card.remove();
+    });
+  }
+}
 
 // @todo: Вывести карточки на страницу
+function renderCard(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    createCard(arr[i]["link"], arr[i]["name"]);
+  }
+}
+
+renderCard(initialCards);
+
